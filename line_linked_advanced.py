228d4fea9701cb6e13cab5620f9095c4a2585851
Paddy Mullen <paddy@paddymullen.com>
2013-11-09 22:46:12 -0500

    linked panning

--- line_linked_advanced.py
+++ line_linked_advanced.py
@@ -3,7 +3,7 @@ from numpy import pi, arange, sin, cos
 import numpy as np
 import os.path
 
-from bokeh.objects import (Plot, DataRange1d, 
+from bokeh.objects import (Plot, DataRange1d, GridPlot,
         ColumnDataSource, GlyphRenderer, PanTool, ZoomTool)
 
 from bokeh.glyphs import Line
@@ -19,42 +19,61 @@ source = ColumnDataSource(data=dict(x=x,y=y,z=z,widths=widths,
             heights=heights))
 
 xdr = DataRange1d(sources=[source.columns("x")])
+xdr2 = DataRange1d(sources=[source.columns("x")])
 ydr = DataRange1d(sources=[source.columns("y")])
+ydr2 = DataRange1d(sources=[source.columns("y")])
 
 line_glyph = Line(x="x", y="y", line_color="blue")
 
 renderer = GlyphRenderer(data_source = source,  xdata_range = xdr,
         ydata_range = ydr, glyph = line_glyph)
+pantool = PanTool(dataranges = [xdr, ydr], dimensions=["width","height"])
+zoomtool = ZoomTool(dataranges=[xdr,ydr], dimensions=("width","height"))
 
 plot = Plot(x_range=xdr, y_range=ydr, data_sources=[source], 
         border=50)
-
+plot.tools = [pantool, zoomtool]
 plot.renderers.append(renderer)
 
+#notice that these two have a differen y data range
 renderer2 = GlyphRenderer(data_source = source, xdata_range = xdr,
-        ydata_range = ydr, glyph = line_glyph)
+        ydata_range = ydr2, glyph = line_glyph)
 
-plot2 = Plot(x_range=xdr, y_range=ydr, data_sources=[source], 
+plot2 = Plot(x_range=xdr, y_range=ydr2, data_sources=[source], 
         border=50)
-pantool2 = PanTool(dataranges = [xdr, ydr], dimensions=["width","height"])
-zoomtool2 = ZoomTool(dataranges=[xdr,ydr], dimensions=("width","height"))
 
 plot2.renderers.append(renderer2)
-plot2.tools = [pantool2, zoomtool2]
 
-sess = session.HTMLFileSession("line_linked.html")
-sess.add(plot, renderer, source, xdr, ydr)
-sess.plotcontext.children.append(plot)
+#notice that these two have a differen y data range
+renderer3 = GlyphRenderer(data_source = source, xdata_range = xdr2,
+        ydata_range = ydr, glyph = line_glyph)
 
-sess.add(plot2, renderer2, pantool2, zoomtool2)
-sess.plotcontext.children.append(plot2)
+plot3 = Plot(x_range=xdr2, y_range=ydr, data_sources=[source], 
+        border=50)
+
+plot3.renderers.append(renderer3)
+
+#this is a dummy plot with no renderers
+plot4 = Plot(x_range=xdr2, y_range=ydr, data_sources=[source], 
+        border=50)
+
+
+sess = session.HTMLFileSession("line_linked_advanced.html")
+sess.add(plot, renderer, source, xdr, ydr, pantool, zoomtool)
+
+sess.add(plot2, renderer2, ydr2, xdr2, renderer3, plot3, plot4)
+grid = GridPlot(children=[[plot, plot2], [plot3, plot4 ]], name="linked_advanced")
+
+sess.add(grid)
+sess.plotcontext.children.append(grid)
+
 
 sess.save(js="relative", css="relative", rootdir=os.path.abspath("."))
-print "Wrote line_linked.html"
+print "Wrote line_linked_advanced.html"
 
 try:
     import webbrowser
-    webbrowser.open("file://" + os.path.abspath("line_linked.html"))
+    webbrowser.open("file://" + os.path.abspath("line_linked_advanced.html"))
 except:
     pass
 
